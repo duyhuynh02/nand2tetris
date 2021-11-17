5a7fc6365504833d8f5096510908c374fb1266b3
Huynh Minh Duy <minhduyhuynh16@gmail.com>
2021-11-17 19:21:06 +0700

    Modify the regular expression for tokenizing word

--- nand2tetris/projects/10/10.3/Lex.py
+++ nand2tetris/projects/10/10.3/Lex.py
@@ -15,6 +15,7 @@ class Lex:
         """Return all the token in each line"""
         result = []
         for word in self._split(self._remove_comments(lines)):
+            # print(word)
             result.append(self._token(word))
         return result 
 
@@ -27,9 +28,9 @@ class Lex:
     _keyword_re = '|'.join(Constant.keywords)
     # print(_keyword_re)
     _integer_re = r'\d+'
-    _string_re = r'"\w*"'
-    _symbol_re = '[' + re.escape(Constant.symbols) + ']' #Not sure about this - will come back later 
-    _identifier_re = r'[\w\_]+'
+    _string_re = r'"[^"\n"]*"'
+    _symbol_re = '[' + re.escape(Constant.symbols) + ']' 
+    _identifier_re = r'[\w\_]+' 
     _word_re = re.compile(_keyword_re + '|' + _symbol_re + '|' + _identifier_re + '|'\
                             + _integer_re + '|' + _string_re)
     def _split(self, line):
@@ -39,14 +40,40 @@ class Lex:
         if self._is_keyword(word):
             # print((Constant.T_KEYWORD, word))
             return (Constant.T_KEYWORD, word)
+        elif self._is_symbol(word):
+            # print((Constant.T_SYM, word))
+            return ((Constant.T_SYM, word))
+        elif self._is_num(word):
+            # print((Constant.T_NUM, word))
+            return (Constant.T_NUM, word)
+        elif self._is_string(word):
+            # print((Constant.T_STR, word))
+            return (Constant.T_STR, word)
+        elif self._is_identifier(word):
+            print((Constant.T_ID, word))
+            return (Constant.T_ID, word)
         else:
             return (Constant.T_ERROR, word)
+
+    def _is_match(self, pattern, string):
+        return re.match(pattern, string) != None 
 
     def _is_keyword(self, word):
         return self._is_match(self._keyword_re, word)
 
-    def _is_match(self, pattern, string):
-        return re.match(pattern, string) == None 
+    def _is_symbol(self, word):
+        return self._is_match(self._symbol_re, word)
+
+    def _is_num(self, word):
+        return self._is_match(self._integer_re, word)
+
+    def _is_string(self, word):
+        return self._is_match(self._string_re, word)
+
+    def _is_identifier(self, word):
+        return self._is_match(self._identifier_re, word)
+
+
 
 
 
